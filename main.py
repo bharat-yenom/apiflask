9a4166973de0c17bf8ec5003912628a849cf6a32
bharat-yenom <bharat@yenom.ai>
2024-03-06 22:49:14 +0530

    create all tables when they do not exist added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
+
+with app.app_context():
+    # Create the database tables
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
