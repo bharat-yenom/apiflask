c07bbdb9edf9f6f081e252271bd40b9701a12613
bharat-yenom <bharat@yenom.ai>
2024-03-06 22:25:40 +0530

    cors updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -9,6 +10,8 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+    
+    CORS(app)
 
     from app.routes import main_bp
     app.register_blueprint(main_bp)
